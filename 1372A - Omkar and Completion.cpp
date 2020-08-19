b1fb73b9111b9ed90516c37597be327c16d9924a
Tuğcan Hoşer <51884151+Tugcannn@users.noreply.github.com>
2020-08-19 15:00:58 +0300

    Codeforces

---
+++ 1372A - Omkar and Completion.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+#include <cmath>
+#include <string>
+#include <bits/stdc++.h>
+#include <algorithm> 
+#include <vector >
+#include <cctype>
+ 
+ 
+using namespace std ;
+ 
+int main(){
+  
+		int t ;
+		cin >> t;
+	while(t--){
+	  
+	  int n ; 
+	  cin >> n ;
+	  for(int i = 0 ; i < n ; i++)
+	  cout << "1" << " " ;
+	  
+	  cout << endl ;
+  }
+    return 0;
+}
